bf9e02de7d883751c3ddd5507479e4042bb1a90c
Maryna-Lasitsyna <121902242+Maryna-Lasitsyna@users.noreply.github.com>
2023-05-14 10:11:20 +0300

    3

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault(); 
+
+  const { elements } = e.currentTarget; 
+
+  
+  if (!elements.email.value || !elements.password.value) {
+    alert("Please fill in all fields");
+    return;
+  }
+
+  
+  const formData = {
+    email: elements.email.value,
+    password: elements.password.value,
+  };
+
+  console.log(formData); 
+
+  form.reset(); 
+});
